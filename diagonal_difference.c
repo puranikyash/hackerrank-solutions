c3982428302e1ae705e5ab9e6480c2b532ec2728
puranikyash <yash.puranik@mitwpu.edu.in>
2025-10-21 18:57:54 +0530

    Update diagonal_difference.c

--- diagonal_difference.c
+++ diagonal_difference.c
@@ -8,8 +8,8 @@ int diagonalDifference(int arr_rows, int arr_columns, int** arr) {
     int secondarySum = 0;
     
     for (int i = 0; i < n; i++) {
-        primarySum += arr[i][i];              // Primary diagonal
-        secondarySum += arr[i][n - 1 - i];    // Secondary diagonal
+        primarySum += arr[i][i];            
+        secondarySum += arr[i][n - 1 - i];   
     }
     
     return abs(primarySum - secondarySum);
@@ -19,13 +19,11 @@ int main() {
     int n;
     scanf("%d", &n);
     
-    // Allocate memory for 2D array
     int** arr = malloc(n * sizeof(int*));
     for (int i = 0; i < n; i++) {
         arr[i] = malloc(n * sizeof(int));
     }
     
-    // Read the matrix
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &arr[i][j]);
@@ -35,7 +33,6 @@ int main() {
     int result = diagonalDifference(n, n, arr);
     printf("%d\n", result);
     
-    // Free memory
     for (int i = 0; i < n; i++) {
         free(arr[i]);
     }
